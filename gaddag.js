bf6b2c79460e067a80beb1c230ed209315617c21
Johan Hillerström <progr@mmer.nu>
2015-03-26 23:54:14 +0100

    Using babel (ES6/7)

--- gaddag.js
+++ gaddag.js
@@ -1,158 +1,166 @@
 /*!
  * gaddag.js
- * Copyright(c) 2012 hillerstorm <progr@mmer.nu>
+ * Copyright(c) 2015 hillerstorm <progr@mmer.nu>
  */
-var fs = require('fs');
+'use strict';
 
-function str (node) {
-    var s, label;
+import fs from 'fs';
 
-    if (node.$) {
-        s = '1';
-    } else {
-        s = '0';
+class Node {
+  constructor(id = 0) {
+    this.id = id;
+    this.edges = {};
+    this.$ = 0;
+  }
+}
+
+// Loads a newline-separated file from disk containing words to use
+export default class Gaddag {
+  constructor (dictionary, onCompleted) {
+    this._root = new Node();
+
+    this._nextId = 1;
+    this._previousWord = '';
+    this._uncheckedNodes = [];
+    this._minimizedNodes = {};
+
+    fs.readFile(dictionary, (err, data) => this._onDictionaryRead(err, data, onCompleted));
+  }
+
+  // Finds a word, returns 1 if the word was found, 0 otherwise
+  find (word) {
+    let node = this._root;
+
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i].toUpperCase();
+      if (!node.edges[letter]) {
+        return 0;
+      }
+      node = node.edges[letter];
     }
 
-    for (label in node.edges) {
-        if (node.edges.hasOwnProperty(label)) {
-            s += '_' + label + '_' + node.edges[label].id;
-        }
+    return node.$;
+  }
+
+  // Returns the subtree matching the letter given
+  get (letter) {
+    return this._root.edges[letter.toUpperCase()];
+  }
+
+  _str (node) {
+    let s = node.$ ? '1' : '0';
+
+    const keys = Object.keys(node.edges);
+    for (let i = 0; i < keys.length; i++) {
+      s += `_${node.edges[keys[i]]}_${node.edges[keys[i]].id}`;
     }
 
     return s;
+  }
+
+  _minimize (downTo) {
+    for (let i = this._uncheckedNodes.length-1; i > downTo-1; i--) {
+      const tuple = this._uncheckedNodes.pop();
+      const childKey = this._str(tuple.child);
+      const node = this._minimizedNodes[childKey];
+      if (node) {
+        tuple.parent.edges[tuple.letter] = node;
+      } else {
+        this._minimizedNodes[childKey] = tuple.child;
+      }
+    }
+  }
+
+  _insert (word) {
+    let commonPrefix = 0;
+    const minWordLength = Math.min(word.length, this._previousWord.length);
+
+    for (let i = 0; i < minWordLength; i++) {
+      if (word[i] !== this._previousWord[i]) {
+        break;
+      }
+      commonPrefix += 1;
+    }
+
+    this._minimize(commonPrefix);
+    var node;
+    if (this._uncheckedNodes.length === 0) {
+      node = this._root;
+    } else {
+      node = this._uncheckedNodes[this._uncheckedNodes.length-1].child;
+    }
+
+    const slicedWord = word.slice(commonPrefix);
+    for (let i = 0; i < slicedWord.length; i++) {
+      const nd = new Node(this._nextId);
+      this._nextId += 1;
+      const ltr = slicedWord[i].toUpperCase();
+      node.edges[ltr] = nd;
+      this._uncheckedNodes.push({
+        parent: node,
+        letter: ltr,
+        child: nd
+      });
+      node = nd;
+    }
+    node.$ = 1;
+    this._previousWord = word;
+  }
+
+  _finish () {
+    this._minimize(0);
+    this._uncheckedNodes = null;
+    this._minimizedNodes = null;
+    this._previousWord = null;
+    this._nextId = null;
+  }
+
+  _onDictionaryRead (err, data, cb) {
+    if (!err) {
+      try {
+        const words = {
+          // TODO: fix a better way of handling huge dictionaries.
+          // The only reason it's saved this way is because
+          // sorting one letter at a time is quicker...
+          a: [], b: [], c: [], d: [],
+          e: [], f: [], g: [], h: [],
+          i: [], j: [], k: [], l: [],
+          m: [], n: [], o: [], p: [],
+          q: [], r: [], s: [], t: [],
+          u: [], v: [], w: [], x: [],
+          y: [], z: [], å: [], ä: [], ö: [], ø: [], æ: []
+        };
+
+        const mapWord = (line) => {
+          return (letter, index, arr) =>
+            words[letter].push(
+              index === 0
+              ? line
+              : line.slice(index, line.length) + '_' + arr.slice(0, index).reverse().join(''));
+        };
+
+        const lines = data.toString().split('\r\n');
+        for (let i = 0; i < lines.length; i++) {
+          lines[i].split('').map(mapWord(lines[i]));
+        }
+
+        const keys = Object.keys(words);
+        for (let i = 0; i < keys.length; i++) {
+          const sorted = words[keys[i]].sort();
+          for (let j = 0; j < sorted.length; j++) {
+            this._insert(sorted[j]);
+          }
+        }
+        this._finish();
+      } catch (e) {
+        err = e;
+      }
+    }
+
+    if (cb) {
+      cb(err);
+    } else if (err) {
+      throw err;
+    }
+  }
 }
-
-module.exports = function () {
-    var nextId = 0,
-        previousWord = '',
-        root = { id: nextId++, edges: {}, $: 0 },
-        uncheckedNodes = [],
-        minimizedNodes = {};
-
-    function minimize (downTo) {
-        var i, tuple,
-            childKey, node;
-
-        for (i = uncheckedNodes.length-1; i > downTo-1; i--) {
-            tuple = uncheckedNodes.pop();
-            childKey = str(tuple.child);
-            node = minimizedNodes[childKey];
-            if (node) {
-                tuple.parent.edges[tuple.letter] = node;
-            } else {
-                minimizedNodes[childKey] = tuple.child;
-            }
-        }
-    }
-
-    function insert (word) {
-        var commonPrefix = 0, i,
-            node, slicedWord, nd, ltr;
-
-        for (i = 0; i < Math.min(word.length, previousWord.length); i++) {
-            if (word[i] !== previousWord[i]) {
-                break;
-            }
-            commonPrefix += 1;
-        }
-
-        minimize(commonPrefix);
-        if (uncheckedNodes.length === 0) {
-            node = root;
-        } else {
-            node = uncheckedNodes[uncheckedNodes.length-1].child;
-        }
-
-        slicedWord = word.slice(commonPrefix);
-        for (i = 0; i < slicedWord.length; i++) {
-            nd = { id: nextId++, edges: {}, $: 0 };
-            ltr = slicedWord[i].toUpperCase();
-            node.edges[ltr] = nd;
-            uncheckedNodes.push({
-                parent: node,
-                letter: ltr,
-                child: nd
-            });
-            node = nd;
-        }
-        node.$ = 1;
-        previousWord = word;
-    }
-
-    function finish () {
-        minimize(0);
-        uncheckedNodes = [];
-        minimizedNodes = {};
-        previousWord = null;
-    }
-
-    // Finds a word, returns 1 if the word was found, 0 otherwise
-    this.find = function (word) {
-        var node = root,
-            i, letter;
-
-        for (i = 0; i < word.length; i++) {
-            letter = word[i].toUpperCase();
-            if (!node.edges[letter]) {
-                return 0;
-            }
-            node = node.edges[letter];
-        }
-        return node.$;
-    };
-
-    // Returns the subtree matching the letter given
-    this.get = function (letter) {
-        return root.edges[letter.toUpperCase()];
-    };
-
-    // Loads a newline-separated file from disk containing words to use
-    this.load = function (dictionary, onCompleted) {
-        process.nextTick(function () {
-            var words = {
-                // TODO: fix a better way of handling huge dictionaries.
-                // The only reason it's saved this way is because
-                // sorting one letter at a time is quicker...
-                a: [], b: [], c: [], d: [],
-                e: [], f: [], g: [], h: [],
-                i: [], j: [], k: [], l: [],
-                m: [], n: [], o: [], p: [],
-                q: [], r: [], s: [], t: [],
-                u: [], v: [], w: [], x: [],
-                y: [], z: [], å: [], ä: [], ö: [], ø: [], æ: []
-            }, letter;
-
-            nextId = 0;
-            previousWord = '';
-            root = { id: nextId++, edges: {}, $: 0 };
-            uncheckedNodes = [];
-            minimizedNodes = {};
-
-            fs.readFileSync(dictionary)
-                .toString()
-                .split('\r\n')
-                .forEach(function (word) {
-                    var i, idx;
-
-                    words[word[0]].push(word);
-                    word += '_';
-                    for (i = 2; i < word.length; i++) {
-                        idx = word.indexOf('_') + 1;
-                        word = word.slice(1, idx) + word[0] + word.slice(idx);
-                        words[word[0]].push(word);
-                    }
-                });
-
-            for (letter in words) {
-                if (words.hasOwnProperty(letter)) {
-                    words[letter].sort();
-                    words[letter].forEach(insert);
-                }
-            }
-            finish();
-            if (onCompleted) {
-                onCompleted();
-            }
-        });
-    };
-};
